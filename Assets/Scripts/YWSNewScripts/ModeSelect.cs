50a560be3349b692042bb0ec7117c52594b6bbab
YWS <zacpccw@yahoo.co.jp>
2021-06-26 09:41:09 +0900

    タイトルからキャラクター選択シーンへの遷移一応完成（あとでコントローラーに対応させる） ちなみに遷移はFadeSceneからプレイしないと使えない

--- Assets/Scripts/YWSNewScripts/ModeSelect.cs
+++ Assets/Scripts/YWSNewScripts/ModeSelect.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ModeSelect : MonoBehaviour
 {
     public RectTransform cursor;
-    int countUp = 0;
-    int countDown = 0;
+    int selectCount = 0;
+    private GameSceneManager _gameSceneManager;
 
     // Start is called before the first frame update
     void Start()
     {
         cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, 0, 0);
+        _gameSceneManager = FindObjectOfType<GameSceneManager>();
     }
 
     // Update is called once per frame
@@ -20,21 +22,49 @@ public class ModeSelect : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (countDown == 0)
+            if (selectCount == 0)
             {
                 cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, -101, 0);
-                countDown++;
+                selectCount++;
             }
-            else if (countDown == 1)
+            else if (selectCount == 1)
             {
                 cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, -202, 0);
-                countDown++;
+                selectCount++;
             }
-            else if (countDown == 2)
+            else if (selectCount == 2)
             {
                 cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, 0, 0);
-                countDown = 0;
+                selectCount = 0;
             }
         }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (selectCount == 0)
+            {
+                cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, -202, 0);
+                selectCount = 2;
+            }
+            else if (selectCount == 1)
+            {
+                cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, 0, 0);
+                selectCount--;
+            }
+            else if (selectCount == 2)
+            {
+                cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, -101, 0);
+                selectCount--;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && selectCount == 1)
+        {
+            SceneChange(_gameSceneManager);
+        }
+    }
+
+    //éüÇÃÉVÅ[ÉìÇ…êiÇﬁ
+    public void SceneChange(GameSceneManager gameSceneManager)
+    {
+        gameSceneManager.SceneNextCall("CharacterSelect");
     }
 }
