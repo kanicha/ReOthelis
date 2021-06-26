ef3715ad5ab7e35310631a4aa400ccfdc33bd10c
YWS <zacpccw@yahoo.co.jp>
2021-06-26 10:06:37 +0900

    キャラクター選択シーンからメインゲームへの遷移もできた（あとはコントローラーに対応させるだけ）

--- Assets/Scripts/YWSNewScripts/ModeSelect.cs
+++ Assets/Scripts/YWSNewScripts/ModeSelect.cs
@@ -9,17 +9,20 @@ public class ModeSelect : MonoBehaviour
     public RectTransform cursor;
     int selectCount = 0;
     private GameSceneManager _gameSceneManager;
+    UnityEvent Approval = new UnityEvent();
 
     // Start is called before the first frame update
     void Start()
     {
         cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, 0, 0);
         _gameSceneManager = FindObjectOfType<GameSceneManager>();
+        Approval.AddListener(() => SceneChange(_gameSceneManager));
     }
 
     // Update is called once per frame
     void Update()
     {
+        //下キー入力に合わせてカーソルを下に移動させる
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             if (selectCount == 0)
@@ -34,14 +37,17 @@ public class ModeSelect : MonoBehaviour
             }
             else if (selectCount == 2)
             {
+                //一番下のONLINEに合わせてる場合は一番上に戻す
                 cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, 0, 0);
                 selectCount = 0;
             }
         }
+        //上キー入力に合わせてカーソルを上に移動させる
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (selectCount == 0)
             {
+                //一番上のSTORYに合わせてる場合は一番下に移す
                 cursor.GetComponent<RectTransform>().anchoredPosition = new Vector3(-210, -202, 0);
                 selectCount = 2;
             }
@@ -56,9 +62,10 @@ public class ModeSelect : MonoBehaviour
                 selectCount--;
             }
         }
+        //カーソルがOFFLINEに合わせている、かつ選択ボタンが押された場合にシーン遷移を行う
         else if (Input.GetKeyDown(KeyCode.Space) && selectCount == 1)
         {
-            SceneChange(_gameSceneManager);
+            Approval.Invoke();
         }
     }
 
--- Assets/Scripts/YWSOldScripts/SceneChange/CharacterSelectScene.cs
+++ Assets/Scripts/YWSOldScripts/SceneChange/CharacterSelectScene.cs
@@ -2,41 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class CharacterSelectScene : MonoBehaviour
 {
-    //上ボタンの取得
-    [SerializeField] private Button _moveOn;
-    public Button MoveOn
-    {
-        get { return _moveOn; }
-    }
-
-    //下ボタンの取得
-    [SerializeField] private Button _goBack;
-    public Button GoBack
-    {
-        get { return _goBack; }
-    }
-
     private GameSceneManager _gameSceneManager;
-
-    //次のシーンに進む
-    public void MoveOnPush(GameSceneManager gameSceneManager)
-    {
-        gameSceneManager.SceneNextCall("CharacterEpisode");
-    }
-
-    //前のシーンに戻る
-    public void GoBackPush(GameSceneManager gameSceneManager)
-    {
-        gameSceneManager.SceneNextCall("Title");
-    }
+    UnityEvent Approval = new UnityEvent();
 
     void Start()
     {
         _gameSceneManager = FindObjectOfType<GameSceneManager>();
-        MoveOn.onClick.AddListener(() => MoveOnPush(_gameSceneManager));
-        GoBack.onClick.AddListener(() => GoBackPush(_gameSceneManager));
+        Approval.AddListener(() => SceneChange(_gameSceneManager));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Approval.Invoke();
+        }
+    }
+
+    //次のシーンに進む
+    public void SceneChange(GameSceneManager gameSceneManager)
+    {
+        gameSceneManager.SceneNextCall("GameSceme");
     }
 }
