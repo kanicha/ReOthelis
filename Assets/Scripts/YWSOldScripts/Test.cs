746f64ed90ec201c89a886ba3b681c48fe7ae4fc
YWS <zacpccw@yahoo.co.jp>
2021-07-09 18:33:04 +0900

    リザルド画面の画面を仮に作った

--- Assets/Scripts/YWSOldScripts/Test.cs
+++ Assets/Scripts/YWSOldScripts/Test.cs
@@ -6,20 +6,5 @@ using UnityEngine.SceneManagement;
 
 public class Test : MonoBehaviour
 {
-    public RectTransform background;
-    private int move = 1;
-    private int distance = 0;
-
-    void Update()
-    {
-        if (distance == 1900)
-        {
-            SceneManager.LoadScene("Result");
-        }
-        else
-        {
-            background.position += new Vector3(0, move, 0);
-            distance = distance + move;
-        }
-    }
+    
 }
