6aa7fb3689b1a3d31cf64359a8227604d3ca24ab
YWS <zacpccw@yahoo.co.jp>
2021-07-17 11:56:13 +0900

    勘違いしてた、スコア表示やり直す

--- Assets/Scripts/YWSOldScripts/ScoreDisplay.cs
+++ Assets/Scripts/YWSOldScripts/ScoreDisplay.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ScoreDisplay : MonoBehaviour
 {
+    public Text P1Score;
+    public Text P2Score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        P1Score.text = "00000";
+        P2Score.text = "00000";
     }
 
     // Update is called once per frame
