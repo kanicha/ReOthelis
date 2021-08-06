9e5cef8ef6c9e7732c93339248291d1237ae047d
somemasa0430 <somemasa0430@docomo.ne.jp>
2021-08-06 09:56:51 +0900

    DebugでCSVファイルを読み込ませた

--- Assets/Scripts/Someya/Loadtext.cs
+++ Assets/Scripts/Someya/Loadtext.cs
@@ -9,19 +9,18 @@ public class Loadtext : MonoBehaviour
     [SerializeField]
     private Text maintext;
     // CSVファイル
-    [SerializeField]
     private TextAsset csvFile;
     // CSVの中身を入れるリスト
     List<string[]> csvDatas = new List<string[]>();
     //一文字一文字の表示する速さ
-    [SerializeField]
-    float novelSpeed;
+    //[SerializeField]
+    //float novelSpeed;
     // CSVの行数
-    public int height; 
+    public int height;
     private void Start()
     {
         // Resouces下のCSV読み込み
-        csvFile = Resources.Load("Resources/Datas/oseris_01") as TextAsset;
+        csvFile = Resources.Load("Datas/oseris_01") as TextAsset;
         StringReader reader = new StringReader(csvFile.text);
 
         // , で分割しつつ一行ずつ読み込み
@@ -34,9 +33,9 @@ public class Loadtext : MonoBehaviour
             // , 区切りでリストに追加
             csvDatas.Add(line.Split(','));
             // 行数加算
-            height++; 
+            height++;
         }
         // csvDatas[行][列]を指定して値を自由に取り出せる
-        Debug.Log(csvDatas[2][1]);
+        Debug.Log(csvDatas[9][0]);
     }
 }
