3989f08441e35c382d68de48e0d0c75d7627279e
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-09-23 15:12:15 +0900

    ゲームシーンスキルウィンドウ修正

--- Assets/Scripts/Sakamaki/SkillWindowControl.cs
+++ Assets/Scripts/Sakamaki/SkillWindowControl.cs
@@ -2,66 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillWindowControl : PlayerBase
+public class SkillWindowControl : MonoBehaviour
 {
-    [SerializeField] GameObject _skillWindow = null;
-    [SerializeField] GameObject _skillWindowTarget = null;
-    [SerializeField, Range(0, 10)] float _moveSpeed = 1f;
-    private Vector3 _windowPos;
-    private Vector3 _targetPos;
-    private Vector3 _lastPos;
+    [SerializeField, Header("スキルウィンドウオブジェクト")] private GameObject _skillWindowObj = null;
 
-    private float _startTime;
-    private float rate;
-    private bool isCheck = false;
-
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        // 初期化処理
-        _windowPos = _skillWindow.transform.position;
-        _targetPos = _skillWindowTarget.transform.position;
-
-        if (_moveSpeed <= 0)
-        {
-            _windowPos = _targetPos;
-        }
+        _animator = _skillWindowObj.GetComponent<Animator>();
     }
 
     public void ShowSkillWindow()
     {
-        StartCoroutine(ShowSkillWindowCoroutine());
+        _animator.SetTrigger("skillWindow");
     }
-
-    IEnumerator ShowSkillWindowCoroutine()
-    {
-        // シーンがロードされた時からの時間 - 初期値
-        var diff = Time.timeSinceLevelLoad - _startTime;
-
-        if (diff > _moveSpeed)
-        {
-            _lastPos = _targetPos;
-            _skillWindow.transform.position = _lastPos;
-        }
-
-        rate = diff / _moveSpeed;
-
-        // フラグ管理でfalseだったら前面にだす、trueだったら引っ込める
-        switch (isCheck)
-        {
-            case false:
-                isCheck = true;
-                _skillWindow.transform.position = Vector3.Lerp(_windowPos, _targetPos, rate);
-                break;
-            case true:
-                isCheck = false;
-                _skillWindow.transform.position = Vector3.Lerp(_targetPos, _windowPos, rate);
-                break;
-            default:
-                break;
-        }
-
-
-        yield return null;
-    }
+    
 }
