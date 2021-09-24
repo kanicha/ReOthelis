00968d5a6467b19cbcb3d8f92ba3b75d750e882d
chaoswings05 <83049802+chaoswings05@users.noreply.github.com>
2021-09-24 13:51:38 +0900

    シナリオ用の画像素材追加、セリフ表示に25文字以上で改行する仕様を追加、キャラ立ち絵が変わる仕様制作中

--- Assets/Scripts/YWSNewScripts/Scenario/CharacterDisplay.cs
+++ Assets/Scripts/YWSNewScripts/Scenario/CharacterDisplay.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterDisplay : MonoBehaviour
 {
+    [SerializeField] private Image _leftCharacter;
+    [SerializeField] private Image _rightCharacter;
+    [SerializeField] private Sprite[] _leftCharacterImage;
+    [SerializeField] private Sprite[] _rightCharacterImage;
+    private int DialogueNum = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rightCharacter.color = new Color(255,255,255,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (TextDisplay._click == true)
+        {
+            _leftCharacter.sprite = _leftCharacterImage[DialogueNum];
+            _rightCharacter.sprite = _rightCharacterImage[DialogueNum];
+            DialogueNum += 1;
+            if (DialogueNum == 3)
+            {
+                _rightCharacter.color = new Color(255,255,255,1);
+            }
+        }
     }
 }
