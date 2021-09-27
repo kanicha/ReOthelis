c0b5dffecf1079efe50e0f98610a4c4924ef169b
chaoswings05 <83049802+chaoswings05@users.noreply.github.com>
2021-09-27 20:20:51 +0900

    ルミナのキャラシナリオでモブが喋っている時、ルミナを暗くしました

--- Assets/Scripts/YWSNewScripts/CharacterScenario/PartB.cs
+++ Assets/Scripts/YWSNewScripts/CharacterScenario/PartB.cs
@@ -317,7 +317,6 @@ public class PartB : Player1Base
                     {
                         _leftCharacter.color = new Color(0.5f,0.5f,0.5f,1);
                     }
-                    _rightCharacter.color = new Color(255,255,255,1);
                 }
                 //セリフの表示
                 _displayTextSpeed++;
