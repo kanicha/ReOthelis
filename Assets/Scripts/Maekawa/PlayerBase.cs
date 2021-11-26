617b16b2fd170b709e9ad8e75bf40bb02da21056
chaoswings05 <83049802+chaoswings05@users.noreply.github.com>
2021-11-26 18:47:50 +0900

    メインゲームのスクリプトを参考にチュートリアル用のゲームシステム制作途中、あとチュートリアルシーンのシースティをルミナに変えた

--- Assets/Scripts/Maekawa/PlayerBase.cs
+++ Assets/Scripts/Maekawa/PlayerBase.cs
@@ -62,18 +62,13 @@ public class PlayerBase : MonoBehaviour
 
     //
     [SerializeField, Header("1マス落下する時間")] private float _fallTime = 0.0f;
-    [SerializeField, Header("2回目の連続回転入力受付時間")] private float _nextButtonTime = 0;
     [SerializeField] protected Text scoreText = null;
     [SerializeField] protected Text myPieceCountText = null;
     [SerializeField] protected Image charactorImage = null;
     [SerializeField] protected GaugeController gaugeController = null;
     [SerializeField] protected SkillCutinControl skillCutinControl = null;
     [SerializeField] protected SkillWindowControl skillWindowControl = null;
-    private float _moveTimeCount = 0.0f;
-    private float _rotateTimeCount = 0.0f;
-    private float _rotateSecondTimeCount = 0.0f;
-    bool _isPush = false;
-    bool _isSecondPush = false;
+    private float _timeCount = 0.0f;
     public bool isMyTurn = false;
     public bool isSkillActive = false;
     public bool isSpSkillActive = false;
@@ -180,7 +175,7 @@ public class PlayerBase : MonoBehaviour
         Vector3 move = Vector3.zero;
         bool isDown = false;
 
-        _moveTimeCount += Time.deltaTime;
+        _timeCount += Time.deltaTime;
 
         // 移動
         if ((_DS4_horizontal_value < 0 && last_horizontal_value == 0) ||
@@ -193,13 +188,13 @@ public class PlayerBase : MonoBehaviour
                  (_DS4_Lstick_vertical_value < 0 && last_Lstick_vertical_value == 0))
         {
             // カウントを初期化
-            _moveTimeCount = 0f;
+            _timeCount = 0f;
             isDown = true;
             move.z = -1;
         }
-        else if (_moveTimeCount >= _fallTime) // 時間落下
+        else if (_timeCount >= _fallTime) // 時間落下
         {
-            _moveTimeCount = 0f;
+            _timeCount = 0f;
             move.z = -1;
         }
 
@@ -226,36 +221,10 @@ public class PlayerBase : MonoBehaviour
     {
         int lastNum = rotationNum;
 
-        if (!_isPush)
-        {
-            if (_DS4_L1_value || _keyBoardLeft)
-            {
-                rotationNum++; // 左回転
-                // クリックした情報
-                _isPush = true;
-            }
-            else if (_DS4_R1_value || _keyBoardRight)
-            {
-                rotationNum += 3; // 右回転(=左に3回転)
-                _isPush = true;
-            }
-        }
-        // 2回目のプッシュ
-        else
-        {
-            if (_DS4_L1_value || _keyBoardLeft)
-            {
-                rotationNum++; // 左回転
-                // クリックした情報
-                _isSecondPush = true;
-            }
-            else if (_DS4_R1_value || _keyBoardRight)
-            {
-                rotationNum += 3; // 右回転(=左に3回転)
-                _isSecondPush = true;
-            }
-        }
-
+        if (_DS4_L1_value || _keyBoardLeft)
+            rotationNum++; // 左回転
+        else if (_DS4_R1_value || _keyBoardRight)
+            rotationNum += 3; // 右回転(=左に3回転)
 
         // 初期値0 左から 1,2,3
         rotationNum %= 4;
@@ -293,7 +262,7 @@ public class PlayerBase : MonoBehaviour
             }
 
             // 壁にあたってる時 + 素早く2回押しでクイックローテート
-            QuickRotate(_rotateTimeCount, _rotateSecondTimeCount, _nextButtonTime);
+            QuickRotate();
             // 壁にあたってる時 + 回転ボタン押しで回転
 
             /*switch (rotationNum)
@@ -314,58 +283,11 @@ public class PlayerBase : MonoBehaviour
     /// <summary>
     /// 2回素早く回転を押したときにコマの色を変える関数
     /// </summary>
-    protected void QuickRotate(float _turnTime,float _secondTurnTime, float _nextButtonTime)
+    protected void QuickRotate()
     {
-        // 計算した値
-        float calTime = 0.0f;
-        
         // 入力面
-        // 一回目のボタンを受け取る (この時点でコマが壁にあたっていることが確定)
-        if (_isPush)
-        {
-            // 一回目押されてから時間を計測
-            _turnTime += Time.deltaTime;
-            
-            Debug.Log("1回目のタイム" + _turnTime);
 
-            // 一回目のボタンの入力が次の受付時間を超えたらフラグをオフ
-            if (_turnTime > _nextButtonTime)
-            {
-                Debug.LogWarning("TimeOutCancel");
-                _isPush = false;
-            }
-                
-            
-            // 2回目押されるのを検知
-            if (_isSecondPush)
-            {
-                // 二回目の時間計測を開始
-                _secondTurnTime += Time.deltaTime;
-                
-                Debug.Log("2回目のタイム" + _secondTurnTime);
-                
-                // 1回目押された時間と2回目を押された時間を計算する
-                if (_turnTime >= _secondTurnTime)
-                    calTime = _turnTime - _secondTurnTime;
-                else
-                    calTime = _secondTurnTime - _turnTime;
-                
-                // _nextButtonTime の値より少なかったらクイックローテート処理
-                if (calTime >= _nextButtonTime)
-                {
-                    Debug.LogWarning("Quick");
-                }
-                // 少なくなかったら各bool変数をfalseにする
-                else
-                {
-                    Debug.LogWarning("Cancel");
-                    _isPush = false;
-                    _isSecondPush = false;
-                }
-            }
-        }
-        
-        /*// 処理面
+        // 処理面
         // コマの色情報をもってくる
         Piece piece1 = controllPiece1.GetComponent<Piece>();
         Piece piece2 = controllPiece2.GetComponent<Piece>();
@@ -377,7 +299,7 @@ public class PlayerBase : MonoBehaviour
             // ピースの情報をいれかえる
             piece1.SkillReverse(false);
             piece2.SkillReverse(false);
-        }*/
+        }
     }
 
     /// <summary>
@@ -404,9 +326,9 @@ public class PlayerBase : MonoBehaviour
             GameDirector.Instance.nextStateCue = GameDirector.GameState.active;
             GameDirector.Instance.gameState = GameDirector.GameState.interval;
         }
-        else if (_moveTimeCount >= _fallTime) // 時間落下
+        else if (_timeCount >= _fallTime) // 時間落下
         {
-            _moveTimeCount = 0f;
+            _timeCount = 0f;
             move.z = -1;
         }
 
--- Assets/Scripts/YWSNewScripts/Tutorial/TutorialDirector.cs
+++ Assets/Scripts/YWSNewScripts/Tutorial/TutorialDirector.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class TutorialDirector : Player1Base
 {
     public Image blackOutImage; //画面を暗くする用のイメージ
+    PiecePatternGeneretor _generator = null;
+    public Vector3 _DEFAULT_POSITION = Vector3.zero;
+    private float _marginTime = 0;
+    private float _timeCount = 0;
+    public GameObject[] _activePieces = new GameObject[2];
     [SerializeField] private Text _explanText;
     private string _showText;
     [SerializeField] private Image _explanImage;
@@ -19,8 +24,8 @@ public class TutorialDirector : Player1Base
         SpinRight,
         MoveLeft,
         MoveRight,
-        PartTwo,
-        PartThree,
+        Reverse,
+        Skill,
     }
 
     // Start is called before the first frame update
@@ -35,40 +40,92 @@ public class TutorialDirector : Player1Base
         switch(tutorialPhase)
         {
             case TutorialPhase.Intro:
-                FadeIn();
-                _explanText.text = "導入パート";
-                //_explanImage.sprite = _showImage[0];
-                if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Space) || _DS4_circle_value || _DS4_cross_value)
-                {
-                    FadeOut();
-                    tutorialPhase = TutorialPhase.SpinLeft;
-                }
-                break;
+            FadeIn();
+            _explanText.text = "導入パート";
+            //_explanImage.sprite = _showImage[0];
+            if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Space) || _DS4_circle_value || _DS4_cross_value)
+            {
+                FadeOut();
+                PieceSet();
+                tutorialPhase = TutorialPhase.SpinLeft;
+            }
+            break;
 
             case TutorialPhase.SpinLeft:
-                if (_DS4_L1_value || Input.GetKeyDown(KeyCode.M))
-                {
-                    base.PieceRotate();
-                }
-                break;
+            if (_DS4_L1_value || Input.GetKeyDown(KeyCode.M))
+            {
+                base.PieceRotate();
+                tutorialPhase = TutorialPhase.SpinRight;
+            }
+            break;
 
             case TutorialPhase.SpinRight:
             if (_DS4_R1_value || Input.GetKeyDown(KeyCode.N))
-                {
-                    base.PieceRotate();
-                }
-                break;
+            {
+                base.PieceRotate();
+                tutorialPhase = TutorialPhase.MoveLeft;
+            }
+            break;
 
             case TutorialPhase.MoveLeft:
-                break;
+            if ((_DS4_horizontal_value < 0 && last_horizontal_value == 0) ||
+                (_DS4_Lstick_horizontal_value < 0 && lastLstick_horizontal_value == 0))
+            {
+                Vector3 move = Vector3.zero;
+                move.x = -1;
+
+                // 移動後の座標を計算
+                Vector3 movedPos = controllPiece1.transform.position + move;
+                Vector3 rotMovedPos = movedPos + rotationPos[rotationNum];
+
+                // 移動後の座標に障害物がなければ
+                if (Map.Instance.CheckWall(movedPos) && Map.Instance.CheckWall(rotMovedPos))
+                {
+                    controllPiece1.transform.position = movedPos;
+                    controllPiece2.transform.position = rotMovedPos;
+                }
+                tutorialPhase = TutorialPhase.MoveRight;
+            }
+            break;
 
             case TutorialPhase.MoveRight:
+            if ((_DS4_horizontal_value > 0 && last_horizontal_value == 0) ||
+                (_DS4_Lstick_horizontal_value > 0 && lastLstick_horizontal_value == 0))
+            {
+                Vector3 move = Vector3.zero;
+                move.x = 1;
+
+                // 移動後の座標を計算
+                Vector3 movedPos = controllPiece1.transform.position + move;
+                Vector3 rotMovedPos = movedPos + rotationPos[rotationNum];
+
+                // 移動後の座標に障害物がなければ
+                if (Map.Instance.CheckWall(movedPos) && Map.Instance.CheckWall(rotMovedPos))
+                {
+                    controllPiece1.transform.position = movedPos;
+                    controllPiece2.transform.position = rotMovedPos;
+                }
+                tutorialPhase = TutorialPhase.Reverse;
+            }
+            break;
+
+            case TutorialPhase.Reverse:
+                FadeIn();
+                if (Map.Instance.CheckLanding(_activePieces[0].transform.position) || Map.Instance.CheckLanding(_activePieces[1].transform.position))
+                {
+                    // 接地時にカウント
+                    _timeCount += Time.deltaTime;
+                    if (_timeCount > _marginTime)
+                    {
+                        _timeCount = 0;
+                    }
+                }
+                else
+                    _timeCount = 0;
+                FadeOut();
                 break;
 
-            case TutorialPhase.PartTwo:
-                break;
-
-            case TutorialPhase.PartThree:
+            case TutorialPhase.Skill:
                 break;
 
             default:
@@ -84,5 +141,33 @@ public class TutorialDirector : Player1Base
     public void FadeOut()
     {
         blackOutImage.color = new Color(0,0,0,0);
+    }
+
+    private void PieceSet()
+    {
+        // 生成位置の1マス下が空いていれば生成
+        Vector3 generatePos = _DEFAULT_POSITION + Vector3.back;
+        int x = 0;
+        while(true)
+        {
+            Vector3 checkPos = generatePos + new Vector3(x, 0);
+            if (Map.Instance.CheckWall(checkPos))
+            {
+                _generator.Generate(checkPos + Vector3.forward);
+                break;
+            }
+            else
+            {
+                checkPos = generatePos + new Vector3(x * -1, 0);
+                if (Map.Instance.CheckWall(checkPos))
+                {
+                    _generator.Generate(checkPos + Vector3.forward);
+                    break;
+                }
+            }
+            x++;
+            if (x > 4)
+                Debug.LogError("生成できるマスがありません");
+        }
     }
 }
