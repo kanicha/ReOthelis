ec14390cf3d6d0ca893d091d4fcb836d5a5f90d2
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-11-26 18:30:32 +0900

    軸のコマが点滅するように修正

---
+++ Assets/Scripts/Sakamaki/Game/EffectController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+    [SerializeField, Header("どれぐらいの間隔で点滅するか")]
+    private float _highLightTime = 0.0f;
+
+    private float _time = 0.0f;
+
+    /// <summary>
+    /// 落下中のコマハイライト表示する関数
+    /// </summary>
+    /// <param name="active">true = on false = off</param>
+    public void FallPieceHighLight(bool active)
+    {
+        // 軸のコマ情報
+        GameObject controlPiece = GameDirector.Instance._activePieces[0];
+        // 軸のコマ情報の子についてあるハイライトするためのオブジェクトを参照
+        GameObject controlPieceHighLightObj = controlPiece.transform.Find("highLightObj").gameObject;
+
+        // コンポーネント習得
+        Piece piece = controlPiece.GetComponent<Piece>();
+
+        // エフェクトのオン
+        if (active)
+        {
+            // 時間計測
+            _time += Time.deltaTime;
+
+            // 黒だった場合座標をずらして表示をおこなう
+            if (piece.pieceType == Piece.PieceType.black)
+            {
+                // 黒コマはエフェクトの座標が反転してしまうため y の値を増加させる
+                Vector3 pos = controlPieceHighLightObj.transform.position;
+                pos.y = -0.1f;
+                controlPieceHighLightObj.transform.position = pos;
+            }
+
+            // 時間がハイライトの時間より大きくなったら
+            if (_time > _highLightTime)
+            {
+                // falseだったらtimeを初期化した上でtrue
+                if (controlPieceHighLightObj.activeSelf == false)
+                {
+                    controlPieceHighLightObj.SetActive(true);
+                    _time = 0;
+                }
+                else
+                {
+                    controlPieceHighLightObj.SetActive(false);
+                    _time = 0;
+                }
+            }
+        }
+        // エフェクトのオフ
+        else
+        {
+            _time = 0;
+            controlPieceHighLightObj.SetActive(false);
+        }
+    }
+}
