deb6706558044d33297c589f741de891693c6733
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-12-24 19:30:18 +0900

    サーバー開発用シーン追加、サーバーからJsonを介して値の受け渡し

---
+++ Assets/Scripts/Sakamaki/Server/RequestBase.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RequestBase
+{
+    // 通信タイプ保存変数
+    public string _packetType = "";
+
+    // 通信タイプ
+    public enum PacketType
+    {
+        Init,
+        Send,
+        Receive,
+        End
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="packetType">通信のタイプ</param>
+    public RequestBase(PacketType? packetType = null) // 型名の後ろに"?"をいれるとnull許容型になる (本来 null は代入できないができるようになる)
+    {
+        if (packetType != null)
+            _packetType = packetType.ToString();
+    }
+
+    /// <summary>
+    /// 受け取ったRequestBase型のデータを文字列化にする関数
+    /// </summary>
+    /// <param name="requestBase">通信タイプ</param>
+    /// <returns></returns>
+    public static string ParseSendData(RequestBase requestBase)
+    {
+        return JsonUtility.ToJson(requestBase);
+    }
+
+    /// <summary>
+    /// Jsonのデータを文字列型に変換する関数
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T JsonToClass<T>(string jsonData) where T : RequestBase // whereで RequestBase自身か、継承している物のみで指定する
+    {
+        return JsonUtility.FromJson<T>(jsonData);
+    }
+}
---
+++ Assets/Scripts/Sakamaki/Server/ServerManager.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class ServerManager : SingletonMonoBehaviour<ServerManager>
+{
+    private Thread _thread;
+    
+    // IPアドレス
+    private string _ipAdress = "tlf93.synology.me";
+    private int _portNumber = 3359;
+    private TcpClient _tcpClient;
+    private NetworkStream _streamKey;
+    
+    private string _message;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitServer();
+        
+        // 並行処理
+        _thread = new Thread(ReceiveMessage);
+        _thread.Start();
+        
+        RequestBase _requestBase = new RequestBase(RequestBase.PacketType.Init);
+        
+        SendMessage(_requestBase);
+    }
+
+    /// <summary>
+    /// サーバーの初期接続 初期化
+    /// </summary>
+    private void InitServer()
+    {
+        // インスタンス化
+        _tcpClient = new TcpClient();
+        // 初回接続(IPAdress, PortNumber)
+        _tcpClient.Connect(_ipAdress, _portNumber);
+        // NetWorkStreamを習得してくる
+        _streamKey = _tcpClient.GetStream(); 
+    }
+
+    /// <summary>
+    /// サーバーに文字列など送信する関数
+    /// </summary>
+    /// <param name="requestBase">送信するオブジェクト (RequestBase型))</param>
+    public void SendMessage(RequestBase requestBase)
+    {
+        string sendMessage = RequestBase.ParseSendData(requestBase);
+        
+        // byte型変数を用意して、アスキーでエンコーディングを行う
+        byte[] bytes = Encoding.ASCII.GetBytes(sendMessage);
+        
+        // データを送信する
+        _streamKey.Write(bytes, 0, bytes.Length);
+        
+        Debug.Log(sendMessage);
+    }
+    
+    /// <summary>
+    /// サーバーからのデータ受け取り関数
+    /// </summary>
+    public void ReceiveMessage()
+    {
+        try
+        {
+            // DataAvailable, サーバーに読み取り可能なデータが有るかどうかを検知する(true || false で帰ってくる)
+            // 読み取り可能なデータがあったらwhile
+            while (true)
+            {
+                // byte型変数を用意 (TCPは8kbがデフォルトらしいので8kb)
+                byte[] buffer = new byte[8192];
+                // Readでbuffer分データを読み取りcountに代入
+                int count = _streamKey.Read(buffer, 0, buffer.Length);
+
+                // カウントが0より多かったら
+                if (count > 0)
+                {
+                    byte[] data = new byte[count];
+
+                    // Copy関数 (コピー元, コピー元の開始位置, コピー先, コピー先の開始位置, コピーする最大値)
+                    // data変数にbufferのデータをcount分コピーしてくる
+                    Array.Copy(buffer, 0, data, 0, count);
+
+                    // 送られてきたデータをJson -> Class(文字列) に変換を行う
+                    InitRequest jsonData = RequestBase.JsonToClass<InitRequest>(Encoding.UTF8.GetString(data));
+                    Debug.Log(jsonData._packetType);
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("ServerError: " + e.ErrorCode);
+        }
+    }
+}
