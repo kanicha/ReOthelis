b791b7e217e61f5dc3cc084e780510a801f3c815
maekawashota0606 <maekawashota0606@gmail.com>
2022-01-27 02:22:03 +0900

    連続リバース時にスコアUIを複数表示できるよう修正

--- Assets/Scripts/Maekawa/ScoreAnimator.cs
+++ Assets/Scripts/Maekawa/ScoreAnimator.cs
@@ -1,48 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreAnimator : SingletonMonoBehaviour<ScoreAnimator>
 {
     [SerializeField]
-    private float _duration = 1 / 60 * 50;// 1/60fpsÇ≈50fÇ©ÇØÇƒè„Ç…à⁄ìÆ
+    private float _moveDuration = 1 / 60 * 50;// 1/60fpsÇ…Ç®ÇØÇÈ50fÇ©ÇØÇƒè„Ç…à⁄ìÆ
     [SerializeField]
     private float _distance = 80;
     [SerializeField]
     private Material[] _materials = new Material[4];
-    [SerializeField]
-    private Text _text = null;
-    [SerializeField]
+    private GameObject _root = null;
+    private Text[] _texts = new Text[_SIZE];
     private Animator _animator = null;
-    private bool _isActive = false;
-    private float _passedDistance = 0;
     private const float _PARENT_WIDTH = 720;
     private const float _PARENT_HEIGHT = 900;
     private const float _CELL_NUM_X = 8;
     private const float _CELL_NUM_Y = 10;
+    private const int _SIZE = 5;
+    private int _idx = 0;
+    private float[] _times = new float[_SIZE];
+
+    private void Start()
+    {
+        _root = transform.gameObject;
+        for (int i = 0; i < _SIZE; i++)
+            _texts[i] = _root.transform.GetChild(i).gameObject.GetComponent<Text>();
+
+    }
 
     private void Update()
     {
-        if(_isActive)
+        for (int i = 0; i < _SIZE; i++)
         {
-            float moveY = _distance * Time.deltaTime * _duration;
-            _passedDistance += moveY;
-            _text.rectTransform.localPosition += Vector3.up * moveY;
+            _times[i] -= Time.deltaTime;
+            if (_times[i] < 0)
+                continue;
 
-            if (_distance < _passedDistance)
-                _isActive = false;
+            float moveY = _distance * Time.deltaTime * _moveDuration;
+            _texts[i].rectTransform.localPosition += Vector3.up * moveY;
         }
     }
 
     public void OnAddScore(CharaImageMoved.CharaType1P type, Vector3 pos, int point)
     {
-        _text.material = _materials[(int)type];
-        _text.text = point.ToString();
-        _text.rectTransform.anchoredPosition = new Vector2(_PARENT_WIDTH / _CELL_NUM_X * (pos.x - 1), _PARENT_HEIGHT / _CELL_NUM_Y * pos.z);
-        Debug.Log(_text.rectTransform.localPosition);
-        _isActive = true;
-        _passedDistance = 0;
+        _texts[_idx].material = _materials[(int)type];
+        _texts[_idx].text = point.ToString();
+        _texts[_idx].rectTransform.anchoredPosition = new Vector2(_PARENT_WIDTH / _CELL_NUM_X * (pos.x - 1), _PARENT_HEIGHT / _CELL_NUM_Y * pos.z);
+        _animator = _texts[_idx].GetComponent<Animator>();
         _animator.Play("Score", 0, 0);
+        //
+        _times[_idx] = _moveDuration;
+        //
+        _idx++;
+        if (_SIZE <= _idx)
+            _idx = 0;
     }
 }
